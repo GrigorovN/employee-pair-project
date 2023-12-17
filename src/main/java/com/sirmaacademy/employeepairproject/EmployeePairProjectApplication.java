87037b825faf6f6c68f5afef0e87783ffd75f22c
Nikola Grigorov <grigorov.ny@gmail.com>
2023-12-17 13:40:08 +0200

    Initial commit. Created entity Employee and connected to employeedb

---
+++ src/main/java/com/sirmaacademy/employeepairproject/EmployeePairProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sirmaacademy.employeepairproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePairProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePairProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sirmaacademy/employeepairproject/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.sirmaacademy.employeepairproject.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Long id;
+
+    @Column(name = "project_id",nullable = false)
+    @NotNull
+    @Positive(message = "Project ID must be a positive or zero value")
+    private Long projectID;
+
+    @Column(name = "date_from",nullable = false)
+    private LocalDate dateFrom;
+
+    @Column(name = "date_to",nullable = false)
+    private LocalDate dateTo;
+}
